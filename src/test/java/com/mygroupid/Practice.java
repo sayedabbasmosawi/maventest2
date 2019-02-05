fe2493a5a4f554858c3a18497fd2ca30c8ac0e1d
sayedabbasmosawi <samosawi7@gmail.com>
2019-02-04 20:55:50 -0500

    my first

---
+++ src/test/java/com/mygroupid/Practice.java
@@ -0,0 +1,17 @@
+package com.mygroupid;
+
+public class Practice {
+
+	public static void main(String[] args) {
+
+		int a = 20;
+		
+
+		for (int i = a; i >= 0; i--) {
+			System.out.println(i);
+
+		}
+
+	}
+
+}
